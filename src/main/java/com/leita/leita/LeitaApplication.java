cf71939e35773cf5146db9ccdf182a0341059f4a
yeonnnnjs <sungyeon52@gmail.com>
2025-01-12 15:10:59 +0900

    init

---
+++ src/main/java/com/leita/leita/LeitaApplication.java
@@ -0,0 +1,13 @@
+package com.leita.leita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leita/leita/LeitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leita.leita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
