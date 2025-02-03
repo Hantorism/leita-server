a36e261fc611c9a9350f5bc9fd7bc13cc1988f18
yeonnnnjs <sungyeon52@gmail.com>
2025-02-03 13:36:26 +0900

    fix: Swagger server list 추가

--- src/main/kotlin/com/leita/leita/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/leita/leita/common/config/SwaggerConfig.kt
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
+import java.util.List
 
 @Configuration
 class SwaggerConfig {
@@ -25,6 +27,12 @@ class SwaggerConfig {
             .`in`(SecurityScheme.In.HEADER)
             .name(HttpHeaders.AUTHORIZATION)
 
+        val servers = List.of(
+            Server().url("https://dev-server.leita.dev/api").description("Dev Server"),
+            Server().url("https://server.leita.dev/api").description("Prod Server"),
+            Server().url("http://localhost:8080/api").description("Local Server")
+        )
+
         val addSecurityItem = SecurityRequirement()
         addSecurityItem.addList("JWT")
 
@@ -32,6 +40,7 @@ class SwaggerConfig {
         components.addSecuritySchemes("JWT", bearerAuth)
 
         return OpenAPI()
+            .servers(servers)
             .components(components)
             .addSecurityItem(addSecurityItem)
             .info(info)
