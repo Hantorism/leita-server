c9c4bed31c6dbc0d6faa888216225ef352f7a86a
yeonnnnjs <sungyeon52@gmail.com>
2025-02-13 01:51:26 +0900

    feat: submit 도메인 추가

---
+++ src/main/kotlin/com/leita/leita/domain/submit/Result.kt
@@ -0,0 +1,10 @@
+package com.leita.leita.domain.submit
+
+enum class Result(val message: String) {
+    CORRECT("맞았습니다"),
+    WRONG("틀렸습니다"),
+    COMPILE_ERROR("컴파일 에러"),
+    RUNTIME_ERROR("런타임 에러"),
+    TIME_OUT("시간 초과"),
+    MEMORY_OUT("메모리 초과");
+}
