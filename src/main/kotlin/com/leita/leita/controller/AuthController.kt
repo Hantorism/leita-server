a45fd5e16adf2151622fe959a759b31c33d5d410
yeonnnnjs <sungyeon52@gmail.com>
2025-03-02 22:31:47 +0900

    feat: BaseResponse 추가

--- src/main/kotlin/com/leita/leita/controller/AuthController.kt
+++ src/main/kotlin/com/leita/leita/controller/AuthController.kt
@@ -1,7 +1,9 @@
 package com.leita.leita.controller
 
+import com.leita.leita.controller.dto.BaseResponse
 import com.leita.leita.controller.dto.auth.request.*
 import com.leita.leita.controller.dto.auth.response.*
+import com.leita.leita.controller.dto.problem.response.SubmitResponse
 import com.leita.leita.service.AuthService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,14 +13,16 @@ import org.springframework.web.bind.annotation.*
 class AuthController(private val authService: AuthService) {
 
     @PostMapping("/oauth")
-    fun register(@RequestBody  request: OAuthRequest): ResponseEntity<JwtResponse> {
+    fun register(@RequestBody  request: OAuthRequest): ResponseEntity<BaseResponse<JwtResponse>> {
         val response = authService.oauth(request)
-        return ResponseEntity.ok(response)
+        val wrappedResponse: BaseResponse<JwtResponse> = BaseResponse("로그인 완료", response)
+        return ResponseEntity.ok(wrappedResponse)
     }
 
     @GetMapping("/info")
-    fun info(): ResponseEntity<InfoResponse> {
+    fun info(): ResponseEntity<BaseResponse<InfoResponse>> {
         val response = authService.info()
-        return ResponseEntity.ok(response)
+        val wrappedResponse: BaseResponse<InfoResponse> = BaseResponse("유저 정보", response)
+        return ResponseEntity.ok(wrappedResponse)
     }
 }
