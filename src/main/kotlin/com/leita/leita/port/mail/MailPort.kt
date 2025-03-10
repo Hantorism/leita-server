4c06c5855391872a4e51ab732eb84e4eecdad4fc
yeonnnnjs <sungyeon52@gmail.com>
2025-03-10 17:26:50 +0900

    feat: 여러개의 메일을 비동기로 보내는 port 추가

--- src/main/kotlin/com/leita/leita/port/mail/MailPort.kt
+++ src/main/kotlin/com/leita/leita/port/mail/MailPort.kt
@@ -2,4 +2,5 @@ package com.leita.leita.port.mail
 
 interface MailPort {
     fun send(mailType: MailType, email: String, vararg args: String)
+    fun sendAll(mailType: MailType, emails: List<String>, vararg args: String)
 }
