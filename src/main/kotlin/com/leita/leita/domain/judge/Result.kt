ec8cc4bb4bc3011ec870e29c9c76e038db05e215
yeonnnnjs <sungyeon52@gmail.com>
2025-03-11 20:45:19 +0900

    feat: result 추가

--- src/main/kotlin/com/leita/leita/domain/judge/Result.kt
+++ src/main/kotlin/com/leita/leita/domain/judge/Result.kt
@@ -6,5 +6,6 @@ enum class Result(val message: String) {
     COMPILE_ERROR("컴파일 에러"),
     RUNTIME_ERROR("런타임 에러"),
     TIME_OUT("시간 초과"),
-    MEMORY_OUT("메모리 초과");
+    MEMORY_OUT("메모리 초과"),
+    UNKNOWN("기타");
 }
