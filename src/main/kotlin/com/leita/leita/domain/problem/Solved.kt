7e9f9510e45823766d61fafd055b641305547ca1
yeonnnnjs <sungyeon52@gmail.com>
2025-04-13 02:56:17 +0900

    fix: Solved 정보 카운트 추가

--- src/main/kotlin/com/leita/leita/domain/problem/Solved.kt
+++ src/main/kotlin/com/leita/leita/domain/problem/Solved.kt
@@ -5,23 +5,21 @@ import jakarta.persistence.*
 @Embeddable
 open class Solved(
 
-    @Column(name = "solved_count")
-    var count: Long = 0,
+    @Column(name = "solved_success_count")
+    var successCount: Long = 0,
+
+    @Column(name = "solved_total_count")
+    var totalCount: Long = 0,
 
     @Column(name = "solved_rate")
     var rate: Double = 0.0,
 ) {
     fun updateSolved(isSolved: Boolean) {
         if (isSolved) {
-            this.count++
-        } else if (this.count > 0) {
-            this.count--
+            this.successCount++
         }
+        this.totalCount++
 
-        this.rate = if (this.count > 0) {
-            ((this.rate * (this.count - 1) + (if (isSolved) 1 else 0)) / this.count) * 100
-        } else {
-            0.0
-        }
+        this.rate = (successCount.toDouble() / totalCount) * 100
     }
 }
