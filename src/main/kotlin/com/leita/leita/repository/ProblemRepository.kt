d3948de1e18804df020ad49402b5bd86c32e23e3
yeonnnnjs <sungyeon52@gmail.com>
2025-04-14 14:30:40 +0900

    fix: 검색 쿼리 수정

--- src/main/kotlin/com/leita/leita/repository/ProblemRepository.kt
+++ src/main/kotlin/com/leita/leita/repository/ProblemRepository.kt
@@ -8,35 +8,62 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProblemRepository: JpaRepository<Problem, Long> {
+interface ProblemRepository : JpaRepository<Problem, Long> {
     @Query(
         value = """
-        SELECT p.*
-        FROM problem p
-        LEFT JOIN (
-            SELECT problem_id, MAX(result) as result
-            FROM judge
-            WHERE user_id = :userId
-            GROUP BY problem_id
-        ) j ON p.id = j.problem_id
-        WHERE 
-            (:search IS NULL OR 
-            (
-                (CAST(:search AS UNSIGNED) IS NOT NULL AND p.id = CAST(:search AS UNSIGNED)) OR 
-                (CAST(:search AS UNSIGNED) IS NULL AND p.title LIKE %:search%)
-            ))
-        AND (
-            :filter IS NULL OR 
-            (:filter = 'SOLVED' AND j.result = 'CORRECT') OR
-            (:filter = 'UNSOLVED' AND (j.result IS NULL OR j.result != 'CORRECT'))
-        )
-        ORDER BY p.id DESC
-        LIMIT :#{#pageable.pageSize} OFFSET :#{#pageable.offset}
-    """,
+            SELECT p.*
+            FROM problem p
+            LEFT JOIN (
+                SELECT problem_id, MAX(result) AS result
+                FROM judge
+                WHERE user_id = :userId
+                GROUP BY problem_id
+            ) j ON p.id = j.problem_id
+            WHERE
+                (
+                    :search IS NULL OR
+                    (
+                        (:search REGEXP '^[0-9]+$' AND p.id = CAST(:search AS UNSIGNED)) OR
+                        (NOT :search REGEXP '^[0-9]+$' AND p.title LIKE CONCAT('%', :search, '%'))
+                    )
+                )
+            AND
+                (
+                    :filter IS NULL OR
+                    (:filter = 'SOLVED' AND j.result = 'CORRECT') OR
+                    (:filter = 'UNSOLVED' AND (j.result IS NULL OR j.result != 'CORRECT'))
+                )
+            ORDER BY p.id DESC
+            LIMIT :#{#pageable.pageSize} OFFSET :#{#pageable.offset}
+        """,
+        countQuery = """
+            SELECT COUNT(*)
+            FROM problem p
+            LEFT JOIN (
+                SELECT problem_id, MAX(result) AS result
+                FROM judge
+                WHERE user_id = :userId
+                GROUP BY problem_id
+            ) j ON p.id = j.problem_id
+            WHERE
+                (
+                    :search IS NULL OR
+                    (
+                        (:search REGEXP '^[0-9]+$' AND p.id = CAST(:search AS UNSIGNED)) OR
+                        (NOT :search REGEXP '^[0-9]+$' AND p.title LIKE CONCAT('%', :search, '%'))
+                    )
+                )
+            AND
+                (
+                    :filter IS NULL OR
+                    (:filter = 'SOLVED' AND j.result = 'CORRECT') OR
+                    (:filter = 'UNSOLVED' AND (j.result IS NULL OR j.result != 'CORRECT'))
+                )
+        """,
         nativeQuery = true
     )
     fun findProblemsByFilter(
-        userId: Long,
+        userId: Long?,
         search: String?,
         filter: String?,
         pageable: Pageable
