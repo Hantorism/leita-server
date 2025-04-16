4eb0009267ea3ad956ceff640ac52046805ea6f4
yeonnnnjs <sungyeon52@gmail.com>
2025-04-17 00:50:36 +0900

    fix: problem description, input, output 엔티티 타입 변경

--- src/main/kotlin/com/leita/leita/service/AuthService.kt
+++ src/main/kotlin/com/leita/leita/service/AuthService.kt
@@ -12,7 +12,6 @@ import com.leita.leita.repository.UserRepository
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.server.ResponseStatusException
 
 @Service
 class AuthService(
