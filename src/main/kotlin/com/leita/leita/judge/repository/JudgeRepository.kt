46caaf3716a473638a390d130d90c02181be40cc
yeonnnnjs <sungyeon52@gmail.com>
2026-04-26 17:47:15 +0900

    fix: resolve server build errors

--- src/main/kotlin/com/leita/leita/judge/repository/JudgeRepository.kt
+++ src/main/kotlin/com/leita/leita/judge/repository/JudgeRepository.kt
@@ -2,6 +2,7 @@ package com.leita.leita.judge.repository
 
 import com.leita.leita.judge.domain.Judge
 import com.leita.leita.judge.domain.JudgeType
+import com.leita.leita.judge.domain.Result
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
@@ -19,13 +20,12 @@ interface JudgeRepository : JpaRepository<Judge, Long> {
         result: Result, 
         id: Long
     ): Judge?
-    }
 
     fun findAllByTypeOrderByCreatedAtDesc(type: JudgeType): List<Judge>
 
     fun findByProblemIdAndUserId(problemId: String, userId: Long): Judge?
 
-    fun findByProblemIdInAndUserIdAndResult(problemIds: Collection<String>, userId: Long, result: com.leita.leita.judge.domain.Result): List<Judge>
+    fun findByProblemIdInAndUserIdAndResult(problemIds: Collection<String>, userId: Long, result: Result): List<Judge>
 
     fun findByProblemIdInAndUserIdAndType(problemIds: Collection<String>, userId: Long, type: JudgeType): List<Judge>
 
